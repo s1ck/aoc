0a7896ccc4a3a3b6188a481fe383b96f2df35379
Martin Junghanns <m.junghanns@mailbox.org>
2021-12-28 17:28:03 +0100

    Day 2

--- src/bin/aoc2021/day2.rs
+++ src/bin/aoc2021/day2.rs
@@ -1,5 +1,7 @@
-type Input = u64;
-type Output = usize;
+use std::{convert::Infallible, str::FromStr};
+
+type Input = Command;
+type Output = u32;
 
 register!(
     "input/day2.txt";
@@ -9,12 +11,57 @@ register!(
     }
 );
 
-fn part1(items: &[Input]) -> Output {
-    0
+pub enum Direction {
+    Forward,
+    Down,
+    Up,
 }
 
-fn part2(items: &[Input]) -> Output {
-    0
+pub struct Command(Direction, u32);
+
+impl FromStr for Command {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let input = line
+            .split_once(' ')
+            .map(|(dir, dist)| {
+                let dist = dist.parse::<u32>().unwrap();
+
+                let dir = match dir {
+                    "forward" => Direction::Forward,
+                    "down" => Direction::Down,
+                    _ /* up */ => Direction::Up,
+                };
+
+                Self(dir, dist)
+            })
+            .unwrap();
+
+        Ok(input)
+    }
+}
+
+fn part1(commands: &[Input]) -> Output {
+    let (x, y) = commands
+        .iter()
+        .fold((0, 0), |(x, y), Command(dir, dist)| match *dir {
+            Direction::Forward => (x + dist, y),
+            Direction::Down => (x, y + dist),
+            Direction::Up => (x, y - dist),
+        });
+    x * y
+}
+
+fn part2(commands: &[Input]) -> Output {
+    let (x, y, _) = commands
+        .iter()
+        .fold((0, 0, 0), |(x, y, aim), Command(dir, dist)| match *dir {
+            Direction::Forward => (x + dist, y + (dist * aim), aim),
+            Direction::Down => (x, y, aim + dist),
+            Direction::Up => (x, y, aim - dist),
+        });
+    x * y
 }
 
 #[cfg(test)]
@@ -25,19 +72,22 @@ mod tests {
 
     #[test]
     fn test_ex() {
-        let input = r#"
-
-        "#;
+        let input = r#"forward 5
+                       down 5
+                       forward 8
+                       up 3
+                       down 8
+                       forward 2"#;
         let (res1, res2) = Solver::run_on(input);
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 150);
+        assert_eq!(res2, 900);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 2322630);
+        assert_eq!(res2, 2105273490);
     }
 
     #[bench]
