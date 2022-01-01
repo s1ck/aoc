a79cbe5169117824eda876c43013de3db05e1cbe
Martin Junghanns <m.junghanns@mailbox.org>
2022-01-01 21:15:47 +0100

    Day 10

--- src/bin/aoc2021/day10.rs
+++ src/bin/aoc2021/day10.rs
@@ -1,20 +1,70 @@
-type Input = u64;
+type Input = String;
 type Output = usize;
 
 register!(
     "input/day10.txt";
-    (input: input!(parse Input)) -> Output {
+    (input: input!(Input)) -> Output {
         part1(&input);
         part2(&input);
     }
 );
 
-fn part1(items: &[Input]) -> Output {
-    0
+fn part1(lines: &[String]) -> Output {
+    lines.iter().map(|line| check_line(line).0).sum::<u32>() as usize
 }
 
-fn part2(items: &[Input]) -> Output {
-    0
+fn part2(lines: &[String]) -> Output {
+    let mut scores = lines
+        .iter()
+        .map(|line| check_line(line))
+        .filter(|(error, _)| *error == 0)
+        .map(|(_, stack)| {
+            stack
+                .iter()
+                .rev()
+                .map(|c| match c {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => unreachable!(),
+                })
+                .fold(0, |prod, n| prod * 5 + n)
+        })
+        .collect::<Vec<_>>();
+
+    scores.sort_unstable();
+    scores[scores.len() / 2]
+}
+
+fn check_line(line: &str) -> (u32, Vec<char>) {
+    let mut stack = vec![];
+
+    let error = line
+        .chars()
+        .map(|c| match c {
+            '(' | '[' | '{' | '<' => {
+                stack.push(c);
+                0
+            }
+            _ => match stack.pop().unwrap() {
+                '(' if c == ')' => 0,
+                '[' if c == ']' => 0,
+                '{' if c == '}' => 0,
+                '<' if c == '>' => 0,
+                _ => match c {
+                    ')' => 3,
+                    ']' => 57,
+                    '}' => 1197,
+                    '>' => 25137,
+                    _ => unreachable!(),
+                },
+            },
+        })
+        .find(|x| *x > 0)
+        .unwrap_or_default();
+
+    (error, stack)
 }
 
 #[cfg(test)]
@@ -26,18 +76,27 @@ mod tests {
     #[test]
     fn test_ex() {
         let input = r#"
-
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
         "#;
         let (res1, res2) = Solver::run_on(input);
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 26397);
+        assert_eq!(res2, 288957);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 364389);
+        assert_eq!(res2, 2870201088);
     }
 
     #[bench]
