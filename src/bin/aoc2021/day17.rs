c9e3870d493c8014a2663eb31953ddca5f0c9729
Martin Junghanns <m.junghanns@mailbox.org>
2022-01-02 17:30:04 +0100

    Day 17

--- src/bin/aoc2021/day17.rs
+++ src/bin/aoc2021/day17.rs
@@ -1,20 +1,91 @@
-type Input = u64;
-type Output = usize;
+use std::{convert::Infallible, str::FromStr};
+
+type Input = Target;
+type Output = i32;
 
 register!(
     "input/day17.txt";
-    (input: input!(parse Input)) -> Output {
+    (input: input!(first input!(parse Input))) -> Output {
         part1(&input);
         part2(&input);
     }
 );
 
-fn part1(items: &[Input]) -> Output {
-    0
+fn part1(target: &Input) -> Output {
+    simulate(target).0
 }
 
-fn part2(items: &[Input]) -> Output {
-    0
+fn part2(target: &Input) -> Output {
+    simulate(target).1
+}
+
+fn simulate(target: &Target) -> (Output, Output) {
+    let (x_min, x_max) = target.x_range;
+    let (y_min, y_max) = target.y_range;
+
+    let mut peak = 0;
+    let mut vels = 0;
+
+    (1..=x_max).for_each(|d_x| {
+        (y_min..=y_min.abs()).for_each(|d_y| {
+            let (hit, y_max) = fire(d_x, d_y, target);
+            if hit {
+                vels += 1;
+                peak = Output::max(peak, y_max);
+            }
+        });
+    });
+
+    (peak, vels)
+}
+
+fn fire(mut x_v: i32, mut y_v: i32, target: &Target) -> (bool, i32) {
+    let mut x = 0;
+    let mut y = 0;
+    let mut peak = y;
+
+    let (x_min, x_max) = target.x_range;
+    let (y_min, y_max) = target.y_range;
+
+    loop {
+        x += x_v;
+        y += y_v;
+
+        x_v -= x_v.signum();
+        y_v -= 1;
+
+        peak = peak.max(y);
+
+        if x >= x_min && x <= x_max && y >= y_min && y <= y_max {
+            return (true, peak);
+        }
+        if x > x_max || y < y_min {
+            return (false, peak);
+        }
+    }
+}
+
+pub struct Target {
+    x_range: (i32, i32),
+    y_range: (i32, i32),
+}
+
+impl FromStr for Target {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (x, y) = line[13..].split_once(", ").unwrap();
+        let x_range = x[2..]
+            .split_once("..")
+            .map(|(l, h)| (l.parse::<i32>().unwrap(), h.parse::<i32>().unwrap()))
+            .unwrap();
+        let y_range = y[2..]
+            .split_once("..")
+            .map(|(l, h)| (l.parse::<i32>().unwrap(), h.parse::<i32>().unwrap()))
+            .unwrap();
+
+        Ok(Self { x_range, y_range })
+    }
 }
 
 #[cfg(test)]
@@ -25,19 +96,17 @@ mod tests {
 
     #[test]
     fn test_ex() {
-        let input = r#"
-
-        "#;
+        let input = r#"target area: x=20..30, y=-10..-5"#;
         let (res1, res2) = Solver::run_on(input);
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 45);
+        assert_eq!(res2, 112);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 5995);
+        assert_eq!(res2, 3202);
     }
 
     #[bench]
