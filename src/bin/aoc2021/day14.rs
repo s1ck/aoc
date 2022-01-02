2f5570ea73b10f6d14e3032fdb04bb8522eac9ea
Martin Junghanns <m.junghanns@mailbox.org>
2022-01-02 15:10:30 +0100

    Day 14

--- src/bin/aoc2021/day14.rs
+++ src/bin/aoc2021/day14.rs
@@ -1,20 +1,87 @@
-type Input = u64;
+use fxhash::{FxBuildHasher, FxHashMap};
+
+use aoc::{lines, PuzzleInput};
+
+type Input = Manual;
 type Output = usize;
 
 register!(
     "input/day14.txt";
-    (input: input!(parse Input)) -> Output {
+    (input: input!(verbatim Input)) -> Output {
         part1(&input);
         part2(&input);
     }
 );
 
-fn part1(items: &[Input]) -> Output {
-    0
+fn part1(items: &Manual) -> Output {
+    iterate(&items.template, &items.rules, 10)
 }
 
-fn part2(items: &[Input]) -> Output {
-    0
+fn part2(items: &Manual) -> Output {
+    iterate(&items.template, &items.rules, 40)
+}
+
+fn iterate(template: &[char], rules: &FxHashMap<(char, char), char>, steps: u32) -> usize {
+    let mut counts = template.array_windows().fold(
+        FxHashMap::with_capacity_and_hasher(template.len(), FxBuildHasher::default()),
+        |mut counts, [left, right]| {
+            *counts.entry((*left, *right)).or_insert(0) += 1;
+            counts
+        },
+    );
+
+    let mut char_counts = template.iter().fold(
+        FxHashMap::with_capacity_and_hasher(template.len(), FxBuildHasher::default()),
+        |mut counts, n| {
+            *counts.entry(*n).or_insert(0) += 1;
+            counts
+        },
+    );
+
+    let mut next = FxHashMap::with_capacity_and_hasher(template.len(), FxBuildHasher::default());
+
+    for _ in 0..steps {
+        next.clear();
+        for (t @ (left, right), count) in &counts {
+            let mid = rules.get(t).unwrap();
+
+            *next.entry((*left, *mid)).or_insert(0) += count;
+            *next.entry((*mid, *right)).or_insert(0) += count;
+
+            *char_counts.entry(*mid).or_insert(0) += count;
+        }
+
+        std::mem::swap(&mut counts, &mut next);
+    }
+
+    char_counts.values().max().unwrap() - char_counts.values().min().unwrap()
+}
+
+pub struct Manual {
+    template: Vec<char>,
+    rules: FxHashMap<(char, char), char>,
+}
+
+impl PuzzleInput for Manual {
+    type Out = Self;
+
+    fn from_input(input: &str) -> Self::Out {
+        let (template, rules) = input.trim().split_once("\n\n").unwrap();
+
+        let template = template.chars().collect::<Vec<_>>();
+
+        let rules = lines(rules)
+            .map(|line| line.split_once(" -> ").unwrap())
+            .map(|(pair, insertion)| {
+                let mut pair = pair.chars();
+                let pair = (pair.next().unwrap(), pair.next().unwrap());
+                let insertion = insertion.chars().next().unwrap();
+                (pair, insertion)
+            })
+            .collect::<FxHashMap<_, _>>();
+
+        Self { template, rules }
+    }
 }
 
 #[cfg(test)]
@@ -26,18 +93,35 @@ mod tests {
     #[test]
     fn test_ex() {
         let input = r#"
+        NNCB
 
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
         "#;
         let (res1, res2) = Solver::run_on(input);
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 1588);
+        assert_eq!(res2, 2188189693529);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 5656);
+        assert_eq!(res2, 12271437788530);
     }
 
     #[bench]
