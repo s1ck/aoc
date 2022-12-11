449a74baea3dbed4ffca9184bc43bc415aa79bc9
Martin Junghanns <m.junghanns@mailbox.org>
2022-12-11 14:19:24 +0100

    Day 11 - Part 2

--- src/bin/aoc2022/day11.rs
+++ src/bin/aoc2022/day11.rs
@@ -1,95 +1,85 @@
 use aoc::{lines, PuzzleInput};
 
 type Input = Monkey;
-type Output = u32;
+type Output = usize;
 
 register!(
     "input/day11.txt";
     (input: input!(blocks Input)) -> Output {
-
         part1(input.clone());
         part2(input);
     }
 );
 
-fn part1(mut monkeys: Vec<Input>) -> Output {
-    for _ in 0..20 {
-        iteration(&mut monkeys);
+fn part1(monkeys: Vec<Input>) -> Output {
+    simulate::<20, _>(monkeys, |worry_level| worry_level / 3)
+}
+
+fn part2(monkeys: Vec<Input>) -> Output {
+    // For each item, we need to maintain the forwarding condition.
+    // We can make use of the following rule in modulo arithmetics:
+    // A MOD C == 0 <=> (A MOD (C * D) MOD C) == 0 and
+    // A MOD D == 0 <=> (A MOD (C * D) MOD D) == 0
+    // I.e., if a number A is divisible by C, A is also divisible
+    // by any multiple of C.
+    //
+    // In order to maintain that condition for each monkey, we can
+    // use the product of all divisors to keep the levels low:
+    // A MOD C == 0 <=> (A % (C * D)) MOD C == 0 and
+    // A MOD D == 0 <=> (A % (C * D)) MOD D == 0
+    let multiple = monkeys.iter().fold(1, |acc, m| acc * m.divisor);
+    simulate::<10_000, _>(monkeys, |worry_level| worry_level % multiple)
+}
+
+fn simulate<const N: usize, F>(mut monkeys: Vec<Input>, f: F) -> usize
+where
+    F: Fn(usize) -> usize,
+{
+    for _ in 0..N {
+        iterate(&mut monkeys, &f);
     }
     monkeys.sort_unstable_by(|a, b| b.inspections.cmp(&a.inspections));
     monkeys[0].inspections * monkeys[1].inspections
 }
 
-fn part2(items: Vec<Input>) -> Output {
-    0
-}
-
-fn iteration(monkeys: &mut [Input]) {
+fn iterate<F>(monkeys: &mut [Input], f: F)
+where
+    F: Fn(usize) -> usize,
+{
     for i in 0..monkeys.len() {
-        for j in 0..(&monkeys[i].curr).len() {
-            // Monkey inspects an item with a worry level of 79.
-            let worry_level = &monkeys[i].curr[j];
+        for j in 0..monkeys[i].items.len() {
+            let worry_level = &monkeys[i].items[j];
             *(&mut monkeys[i].inspections) += 1;
-            // Worry level is multiplied by 19 to 1501.
-            let worry_level = match (&monkeys[i]).operation {
+            let worry_level = match monkeys[i].operation {
                 Op::Square => worry_level * worry_level,
                 Op::Mul(n) => worry_level * n,
                 Op::Add(n) => worry_level + n,
             };
-            // Monkey gets bored with item. Worry level is divided by 3 to 500.
-            let worry_level = worry_level / 3;
-            // Current worry level is not divisible by 23.
-            if worry_level % (&monkeys[i]).divisor == 0 {
-                monkeys[(&monkeys[i]).targets.0].curr.push(worry_level);
+            let worry_level = f(worry_level);
+            if worry_level % monkeys[i].divisor == 0 {
+                monkeys[monkeys[i].targets.0].items.push(worry_level);
             } else {
-                monkeys[(&monkeys[i]).targets.1].curr.push(worry_level);
+                monkeys[monkeys[i].targets.1].items.push(worry_level);
             }
         }
-        (&mut monkeys[i].curr).clear();
+        monkeys[i].items.clear();
     }
-
-    // prepare monkeys for next round
-    // monkeys.iter_mut().for_each(|m| {
-    //     std::mem::swap(&mut m.curr, &mut m.next);
-    //     m.next.clear();
-    // });
-
-    // debug
-    // println!();
-    // monkeys
-    //     .iter()
-    //     .enumerate()
-    //     .for_each(|(i, m)| println!("Monkey {i}: {:?}", m.curr,))
 }
 
 #[derive(Debug, Clone)]
 pub enum Op {
     Square,
-    Mul(u32),
-    Add(u32),
+    Mul(usize),
+    Add(usize),
 }
 
 #[derive(Debug, Clone)]
 pub struct Monkey {
-    curr: Vec<u32>,
-    next: Vec<u32>,
+    items: Vec<usize>,
     operation: Op,
-    divisor: u32,
-    inspections: u32,
+    divisor: usize,
+    inspections: usize,
     targets: (usize, usize),
-}
-
-impl Monkey {
-    pub fn new(items: Vec<u32>, operation: Op, divisor: u32, targets: (usize, usize)) -> Self {
-        Self {
-            curr: items,
-            next: vec![],
-            inspections: 0,
-            divisor,
-            operation,
-            targets,
-        }
-    }
 }
 
 impl PuzzleInput for Monkey {
@@ -103,8 +93,8 @@ impl PuzzleInput for Monkey {
             .map(|l| l.split_once(':').unwrap().1)
             .map(|nums| {
                 nums.trim()
-                    .split(",")
-                    .map(|n| n.trim().parse::<u32>().unwrap())
+                    .split(',')
+                    .map(|n| n.trim().parse::<usize>().unwrap())
                     .collect::<Vec<_>>()
             })
             .unwrap_or_default();
@@ -115,7 +105,7 @@ impl PuzzleInput for Monkey {
             .map(|op| {
                 let (op, n) = op.split_once(' ').unwrap();
                 match op {
-                    "*" => n.parse().map(|n| Op::Mul(n)).unwrap_or(Op::Square),
+                    "*" => n.parse().map(Op::Mul).unwrap_or(Op::Square),
                     "+" => Op::Add(n.parse().unwrap()),
                     _ => unreachable!(),
                 }
@@ -125,7 +115,7 @@ impl PuzzleInput for Monkey {
         let divisor = lines // Test: divisible by 23
             .next()
             .map(|l| l.split_once("by ").unwrap().1)
-            .map(|n| n.parse::<u32>().unwrap())
+            .map(|n| n.parse::<usize>().unwrap())
             .unwrap_or_default();
 
         let true_target = lines // If true: throw to monkey i
@@ -140,7 +130,13 @@ impl PuzzleInput for Monkey {
             .map(|n| n.parse::<usize>().unwrap())
             .unwrap();
 
-        Self::new(items, operation, divisor, (true_target, false_target))
+        Self {
+            items,
+            operation,
+            divisor,
+            inspections: 0,
+            targets: (true_target, false_target),
+        }
     }
 }
 
@@ -183,14 +179,14 @@ mod tests {
         "#;
         let (res1, res2) = Solver::run_on(input);
         assert_eq!(res1, 10605);
-        assert_eq!(res2, 0);
+        assert_eq!(res2, 2713310158);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
         assert_eq!(res1, 50616);
-        assert_eq!(res2, 0);
+        assert_eq!(res2, 11309046332);
     }
 
     #[bench]
